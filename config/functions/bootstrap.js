d16ce235f6ca2563d59ccc65da9e92fb992093e4
EsraaFarhat <esraafarhat97@gmail.com>
2022-03-25 00:07:01 +0200

    Update permissions for public role

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -10,4 +10,48 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+const findPublicRole = async () => {
+  const result = await strapi
+    .query("role", "users-permissions")
+    .findOne({ type: "public" });
+  return result;
+};
+
+const setDefaultPermissions = async () => {
+  const role = await findPublicRole();
+  let permissions = await strapi
+    .query("permission", "users-permissions")
+    .find({ type: "application", role: role.id });
+
+  permissions = permissions.filter(
+    (p) => p.action === "find" || p.action === "findone"
+  );
+
+  await Promise.all(
+    permissions.map((p) => {
+      return strapi
+        .query("permission", "users-permissions")
+        .update({ id: p.id }, { enabled: true });
+    })
+  );
+};
+
+const isFirstRun = async () => {
+  const pluginStore = strapi.store({
+    environment: strapi.config.environment,
+    type: "type",
+    name: "setup",
+  });
+  const initHasRun = await pluginStore.get({ key: "initHasRun" });
+  await pluginStore.set({ key: "initHasRun", value: true });
+  return !initHasRun;
+};
+
+module.exports = async () => {
+//   const shouldSetDefaultPermissions = await isFirstRun();
+//   if (shouldSetDefaultPermissions) {
+    await setDefaultPermissions();
+//   }
+};
+
+// module.exports = () => {};
