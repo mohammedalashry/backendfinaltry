3b081fdedc295a86b550725a987ac42dbe42daf2
EsraaFarhat <esraafarhat97@gmail.com>
2022-03-31 11:38:33 +0200

    Update database configration for deployment

--- config/database.js
+++ config/database.js
@@ -1,19 +1,13 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST'),
-        srv: env.bool('DATABASE_SRV'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        uri: env("DATABASE_URI"),
       },
       options: {
-        // authenticationDatabase: env('AUTHENTICATION_DATABASE'),
-        ssl: env.bool('DATABASE_SSL'),
+        ssl: true,
       },
     },
   },
